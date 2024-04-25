f94d2fbfa01887c3771ef727bc4226185f76b50e
Steve Hay <github@stevenhay.com>
2024-04-25 16:41:52 -0400

    fix email submission check.

--- quiz.js
+++ quiz.js
@@ -378,7 +378,10 @@ document.addEventListener('DOMContentLoaded', function() {
         let resultsHTML = `<p><div id="userForm"><input type="text" id="name" placeholder="Enter your name"><input type="email" id="email" placeholder="Enter your email"><button id="emailbtn">Submit</button></div>`;
         resultsHTML += `<p>Privacy: We are not storing your scores, we are simply signing you up for Sadie’s very occasional newsletter. You can unsubscribe at any time.`;
         resultsContainer.innerHTML = resultsHTML;
-        document.getElementById('emailbtn').addEventListener('click', submitResults);
+        document.getElementById('emailbtn').addEventListener('submit', function(event) {
+            event.preventDefault();
+            submitResults();
+        });
     }
 
     function submitResults() {
